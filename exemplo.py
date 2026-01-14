ce954b601bb7179f1e41ad4cc82e4056cb748fe0
rscopim <ricardo.simines@gmail.com>
2026-01-14 08:52:03 -0300

    Commit inicial

---
+++ exemplo.py
@@ -0,0 +1,15 @@
+'''idade = "name"
+print(type(idade))
+
+num1 = int(input("Digite o primeiro número: "))
+
+preco = 7
+print(f"Preço: {preco:03}")'''
+
+nota = float(input("Digite a sua note: "))
+if nota >= 700:
+    print("Aprovado")
+elif nota >= 500:
+    print("Retake")
+else:
+    print("reprovado")
